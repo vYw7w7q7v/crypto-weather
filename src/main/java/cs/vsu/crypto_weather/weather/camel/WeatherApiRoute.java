badf068276acdde9e0b258d6182e71b3a3461e18
egor_egor <egorgolovin1@mail.ru>
2024-07-04 15:12:52 +0300

    add jsonpath

--- src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
+++ src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
@@ -4,13 +4,14 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JdbcRoute extends RouteBuilder {
+public class WeatherApiRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:mytimer?repeatCount=1")
+        from("timer:mytimer?period=10000")
                 .toD("http://api.weatherapi.com/v1/current.json?" +
-                        "key=813eff2b8a174601b28101610240407&q=Moscow&aqi=no" +
-                        "?httpMethod=GET")
+                        "key=813eff2b8a174601b28101610240407&q=Moscow&aqi=no?" +
+                        "httpMethod=GET")
+                .split().jsonpath("$.location.name")
                 .log(body().toString());
 
     }
