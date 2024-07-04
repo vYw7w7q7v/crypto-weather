14ce25932a4ec9b128a8618c1311003a7910edc9
egor_egor <egorgolovin1@mail.ru>
2024-07-04 15:36:24 +0300

    add primary jolt transformation

--- src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
+++ src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
@@ -3,6 +3,8 @@ package cs.vsu.crypto_weather.weather.camel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class WeatherApiRoute extends RouteBuilder {
     @Override
@@ -11,7 +13,10 @@ public class WeatherApiRoute extends RouteBuilder {
                 .toD("http://api.weatherapi.com/v1/current.json?" +
                         "key=813eff2b8a174601b28101610240407&q=Moscow&aqi=no?" +
                         "httpMethod=GET")
-                .split().jsonpath("$.location.name")
+                .log(body().toString())
+                .to("jolt:file://src/main/java/cs/vsu/crypto_weather/weather/camel/jolt1.json?" +
+                        "inputType=JsonString&outputType=JsonString")
+                //.split().jsonpath("$.*[*]")
                 .log(body().toString());
 
     }
