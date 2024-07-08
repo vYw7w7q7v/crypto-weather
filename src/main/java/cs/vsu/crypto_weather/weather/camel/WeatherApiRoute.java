cbf6791c7183ab713f2340b8990f260e15a4cbff
egor_egor <egorgolovin1@mail.ru>
2024-07-08 15:47:07 +0300

    extract strings to final variables

--- src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
+++ src/main/java/cs/vsu/crypto_weather/weather/camel/WeatherApiRoute.java
@@ -1,46 +1,40 @@
 package cs.vsu.crypto_weather.weather.camel;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.MessageFormat;
 
 @Component
+@RequiredArgsConstructor
 public class WeatherApiRoute extends RouteBuilder {
 
-    @Autowired
-    private WeatherDataTransformationProcessor transformationProcessor;
-    @Autowired
-    private WeatherDataLoadProcessor loadProcessor;
+    private final WeatherDataTransformationProcessor transformationProcessor;
+    private final WeatherDataLoadProcessor loadProcessor;
 
     @Value("${weather_apikey}")
     private String apikey;
 
-    private String cityList = "Moscow;Dubai;London";
+    private final String timerUri = "timer:weather_data_timer?period=10000&repeatCount=2",
+            weatherApiRouteId = "weather_data_route", cityList = "Moscow;Dubai;London",
+            splitToken = ";", cityNamePattern = "${body}",
+            externalApiPattern = "http://api.weatherapi.com/v1/current.json?key={0}&q={1}&aqi=no?httpMethod=GET";
 
-    private String getConfiguredExternalWeatherApi(String cityName) {
-        return String.format(
-                "http://api.weatherapi.com/v1/current.json?" +
-                "key=%s" +
-                "&q=%s" +
-                "&aqi=no?httpMethod=GET",
-                apikey, cityName);
+    private String getConfiguredExternalWeatherApi() {
+        return MessageFormat.format(externalApiPattern, apikey, cityNamePattern);
     }
 
     @Override
-    public void configure() throws Exception {
+    public void configure() {
 
-        from("timer:weather_data_timer?period=10000&repeatCount=3")
-                .routeId("weather_data_route")
+        from(timerUri)
+                .routeId(weatherApiRouteId)
                 .process(exchange -> exchange.getIn().setBody(cityList))
-                .split(body().tokenize(";"))
+                .split(body().tokenize(splitToken))
                     .log(body().toString())
-                .toD(getConfiguredExternalWeatherApi("${body}"))
+                .toD(getConfiguredExternalWeatherApi())
                 .process(transformationProcessor)
                     .log(body().toString())
                 .process(loadProcessor)
