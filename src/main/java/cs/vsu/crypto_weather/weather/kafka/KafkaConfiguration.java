350ea820190ebd72cb6888f232698465cee115c0
bdybgs <bondarev.mc.2018@gmail.com>
2024-07-10 16:17:03 +0300

    fixed kafka server ports

--- src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaConfiguration.java
+++ src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaConfiguration.java
@@ -1,14 +1,30 @@
 package cs.vsu.crypto_weather.weather.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
 
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
     @Bean
     public NewTopic newTopic() {
-        return new NewTopic("weather", 1,(short) 1);
+        return new NewTopic("weather", 1, (short) 1);
     }
 }
