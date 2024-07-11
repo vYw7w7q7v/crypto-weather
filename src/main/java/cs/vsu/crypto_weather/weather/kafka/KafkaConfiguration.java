5f6bb7d76499080556f9fb0344a174a7206a5ccf
egor_egor <egorgolovin1@mail.ru>
2024-07-11 15:51:32 +0300

    add KafkaProducer and JsonSerializer

--- src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaConfiguration.java
+++ src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaConfiguration.java
@@ -1,27 +1,36 @@
 package cs.vsu.crypto_weather.weather.kafka;
 
+import cs.vsu.crypto_weather.weather.entity.WeatherData;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class KafkaConfiguration {
 
+    @Bean
+    private static KafkaProducer<String, WeatherData> kafkaProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+//        props.put(ProducerConfig.CLIENT_ID_CONFIG, CONSUMER_APP_ID);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class.getName());
+
+        return new KafkaProducer<>(props);
+    }
+
     @Value(value = "${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
 
     @Bean
     public NewTopic newTopic() {
--- src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaSender.java
+++ src/main/java/cs/vsu/crypto_weather/weather/kafka/KafkaSender.java
@@ -3,16 +3,24 @@ package cs.vsu.crypto_weather.weather.kafka;
 import cs.vsu.crypto_weather.weather.entity.WeatherData;
 import cs.vsu.crypto_weather.weather.service.WeatherDataService;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaSender {
     private final KafkaTemplate<String, WeatherData> kafkaTemplate;
+    private final KafkaProducer<String, WeatherData> kafkaProducer;
 
     private final WeatherDataService weatherDataService;
     public void sendMessage() {
@@ -26,6 +34,7 @@ public class KafkaSender {
                 .withPayload(weatherData)
                 .setHeader(KafkaHeaders.TOPIC, "weather")
                 .build();
-        kafkaTemplate.send(message);
+        //kafkaTemplate.send(message);
+        kafkaProducer.send(new ProducerRecord<>("weather", weatherData));
     }
 }
